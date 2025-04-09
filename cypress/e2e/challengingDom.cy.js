4fa4c4355dfdada0c800ca777f6c0898ad434d8b
Ariel Jusid <arieljusid@gmail.com>
2025-04-09 18:07:26 +0700

    tests added

--- cypress/e2e/challengingDom.cy.js
+++ cypress/e2e/challengingDom.cy.js
@@ -1,14 +1,17 @@
 describe('Challenging DOM', () => {
-    beforeEach(()=> {
+    beforeEach(() => {
         cy.visit('/challenging_dom')
-    })
-    it('should interact with the challenging DOM elements', () => {
-        // Test implementation goes here
-
-        cy.get('.button').not('.alert, .success')
-        cy.get('.button').filter('.alert')
-        cy.get('.button').filter('.success')
     });
 
-    it()
+    it('should interact with the challenging DOM elements', () => {
+        // Test implementation goes here
+        cy.get('.button').not('.alert, .success');
+        cy.get('.button').filter('.alert');
+        cy.get('.button').filter('.success');
+    });
+
+    it('should perform another action', () => {
+        // Implement another test or remove this block if unnecessary
+        cy.get('table').should('be.visible');
+    });
 });
